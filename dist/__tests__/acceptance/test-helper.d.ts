78bebd7c60d636eafcbb5329f568be21c9c2bf85
linux <cokan1985@yahoo.com>
2020-06-06 02:12:26 -0700

    Project Initialized

---
+++ dist/__tests__/acceptance/test-helper.d.ts
@@ -0,0 +1,7 @@
+import { GisApiApplication } from '../..';
+import { Client } from '@loopback/testlab';
+export declare function setupApplication(): Promise<AppWithClient>;
+export interface AppWithClient {
+    app: GisApiApplication;
+    client: Client;
+}
